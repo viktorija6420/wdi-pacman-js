44764fe92e67d20df1e5423abba9f0b3a59f6e62
viktorija6420 <viktorija.gjorgjievska@gmail.com>
2017-09-26 17:16:47 -0400

    create array of ghosts, add ghosts to the menu, process eating an inedible ghost, process game over, define powePellets, add eat powerPellet to the menu, process powerPellets, make it impossible to use p when powerPellets are zero, edible ghosts, show in menu if the ghost is edible or inedible

--- pacman.js
+++ pacman.js
@@ -1,6 +1,7 @@
 // Setup initial game stats
 var score = 0;
 var lives = 2;
+var powerPellets = 4;
 
 
 // Define your ghosts here
@@ -36,6 +37,8 @@ var clyde = {
   edible: false
 };
 // replace this comment with your four ghosts setup as objects
+var ghosts = [inky, blinky, pinky, clyde];
+
 
 
 // Draw the screen functionality
@@ -54,12 +57,24 @@ function clearScreen() {
 
 function displayStats() {
   console.log('Score: ' + score + '     Lives: ' + lives);
+  console.log('');
+  console.log('Power-Pellets: ' + powerPellets);
 }
 
 function displayMenu() {
   console.log('\n\nSelect Option:\n');  // each \n creates a new line
   console.log('(d) Eat Dot');
+  console.log('(p) Eat Power-Pellet');
   console.log('(q) Quit');
+  for (var i = 0; i < ghosts.length; i++) {
+    if (ghosts[i].edible === true) {
+    var toEatOrNotToEat='edible'
+  }
+    else
+    var toEatOrNotToEat='inedible'
+    console.log('('+ ghosts[i].menu_option + ')'+ ' ' + 'Eat'+ghosts[i].name + ' ' + '(' + toEatOrNotToEat+')');
+  }
+
 }
 
 function displayPrompt() {
@@ -74,10 +89,26 @@ function eatDot() {
   score += 10;
 }
 
+function eatPowerPellet() {
 
-// Process Player's Input
+
+  if (powerPellets <= 0) {
+  console.log('No more pallets left!');
+}else{
+  score += 50;
+  for (var i = 0; i < ghosts.length; i++) {
+       ghosts[i].edible = true;
+    }
+   powerPellets -= 1;
+}
+
+}
+
+ // Process Player's Input
 function processInput(key) {
   switch(key) {
+    default:
+      console.log('\nInvalid Command!');
     case '\u0003': // This makes it so CTRL-C will quit the program
     case 'q':
       process.exit();
@@ -85,13 +116,39 @@ function processInput(key) {
     case 'd':
       eatDot();
       break;
-    default:
-      console.log('\nInvalid Command!');
+    case 'p':
+      eatPowerPellet();
+     break;
+    case '1':
+     eatGhost(inky);
+     break;
+    case '2':
+     eatGhost(blinky);
+     break;
+    case '3':
+     eatGhost(pinky);
+     break;
+    case '4':
+      eatGhost(clyde);
+      break;
   }
 }
 
+function eatGhost(ghost) {
+  if (ghost.edible === false) {
+    lives -= 1
+    if (lives === 0) {
+    process.exit();
+  }
+    console.log('Ghost killer: name ' + ghost.name + 'colour:' + ghost.colour)}
+  else
+    console.log('Ghost' +ghost.name + 'was a good ghost.');
+    score += 200;
+    ghost.edible = false;
+}
 
-//
+
+
 // YOU PROBABLY DON'T WANT TO CHANGE CODE BELOW THIS LINE
 //
 
